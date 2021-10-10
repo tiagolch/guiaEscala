78f4ac525f0a3488724eb0d9c2ac4dbf86469afc
m2b-tiagochaves <tiagodevch@gmail.com>
2021-10-10 17:12:05 -0300

    Alterado footer e autenticação
    Signed-off-by: m2b-tiagochaves <tiagodevch@gmail.com>

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django import urls
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 from rest_framework import routers
 from escala.views import EscalaViewSet, EventoViewSet
 from ministerio.views import MinisterioViewSet, FuncaoViewSet
@@ -16,5 +17,6 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('admin/', admin.site.urls),
     path('', include('escala.urls')),
+    path('feedback/', TemplateView.as_view(template_name='feedback.html'), name='feedback'),
     path('api/', include(router.urls)),
 ]
