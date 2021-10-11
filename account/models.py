0bec9424dc86e7fe7f132e4624d7df518b8cef91
m2b-tiagochaves <tiagodevch@gmail.com>
2021-10-10 21:11:21 -0300

    admin ministerio
    Signed-off-by: m2b-tiagochaves <tiagodevch@gmail.com>

--- account/models.py
+++ account/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from ministerio.models import Funcao
+from ministerio.models import Funcao, Ministerio
 
 
 class User(AbstractUser):
+    ministerio = models.ForeignKey(Ministerio, on_delete=models.DO_NOTHING)
     funcao = models.ManyToManyField(Funcao)
 
 
