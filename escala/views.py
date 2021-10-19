8203a123c3095ca40085f57f163dc5bd7d8fa8f4
m2b-tiagochaves <tiagodevch@gmail.com>
2021-10-19 11:09:08 -0300

    Alterado templates
    Signed-off-by: m2b-tiagochaves <tiagodevch@gmail.com>

--- escala/views.py
+++ escala/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render,redirect, get_object_or_404
 from datetime import date, timedelta
 from account.models import User
 from rest_framework import viewsets
+
+from ministerio.models import Ministerio
 from .models import Escala, Evento
 from .serializers import EscalaSerializer, EventoSerializer
 
@@ -32,8 +34,19 @@ def MinhaEscalaView(request):
 
 @login_required() 
 def EscalaView(request):
+    busca = request.GET.get('buscar')
+    if busca:
+        result = Escala.objects.filter(nome__icontains=busca, funcao__icontains=busca)
+        paginator = Paginator(result, 50)
+        page = request.GET.get('page')
+        posts = paginator.get_page(page)
+        result = {
+            'core': posts
+        }
     hoje = date.today()
     resultado = Escala.objects.filter(data__month=str(hoje.month))
+    print([resultado])
+
 
     context = {
         'escala': resultado,
